5662cf845ae444e679f7aa37f68b9597410298fa
martinH-R <martin.herve-ravel@hei.yncrea.fr>
2022-12-25 13:36:21 -0800

    adding the creation of the transcriptions dataframe, working fine

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import gradio as gr
 from pytube import YouTube
+from pytube import extract
 from youtube_transcript_api import YouTubeTranscriptApi
 import pandas as pd
 import re
 
-def islinkcompatible (link):
-    #TODO: complete function
+def is_the_input_text_a_youtube_link(link):
+    if re.findall('youtu',link):
+        link=YouTube(link).streams.filter(only_audio=True).get_highest_resolution
+        print("is_the_input_text_a_youtube_link(link) ")
+        return()
+    else:
+        print("this link is not a youtube link")
+
+def get_youtube_id(link):
+    video_id=extract.video_id(link)
+    print("the video id is"+video_id)
+    return(video_id)
+
+def get_subtitles_from_video (link):
+    subtitles = YouTubeTranscriptApi.get_transcript(get_youtube_id(link))
+    print(subtitles)
+    return(subtitles)
+
+def formatting_csv(link):
+    df=pd.DataFrame(get_subtitles_from_video(link))
+    video_names_list=[]
+    value=""
+    for i in range (len(df)):
+        video_names_list.append("video_part_"+str(i))
+    df.insert(0,"video name",video_names_list)
+    print(df)
     return()
 
-def transcriptvideo (link):
-    #TODO: complete function
-    return()
-
-def exporttranscription(link):
-    #TODO: complete function
+def export_subtitles_and_audio(link):
+    #TODO: exporting archive with csv file and multiple audio files in it
     return ()
 
 def input(link):
-    istheinputtextayoutubelink=re.findall('youtu',link)
-    if istheinputtextayoutubelink:
-        link=YouTube(link).streams.filter(only_audio=True).get_highest_resolution
-        subtitles = YouTubeTranscriptApi.get_transcript("SW14tOda_kI")
-        print(subtitles)
-        return {"output_text":subtitles, "output_text":gr.update(visible=True)}
-    else:
-        print("not a youtube link")
-        return{"output_text": "The link is not a youtube link","output_text":gr.update(visible=True)}
+    is_the_input_text_a_youtube_link(link)
+    formatting_csv(link)
+
+
 
 with gr.Blocks(css="#button {width:200px;}") as demo:
     #TODO: adjusting button size in CSS
     with gr.Row():
         link = gr.Textbox(placeholder="Collez votre lien Youtube ici",max_lines=1, show_label=False)
         input_btn = gr.Button("Générer", elem_id="button")
-    output_text = gr.Textbox(max_lines=1, show_label=False, visible=False)
+    output_text = gr.Textbox(placeholder="Le résumé de votre vidéo ici",max_lines=1, show_label=False)
     input_btn.click(fn=input, inputs=link, outputs=output_text)
 
 demo.launch()
